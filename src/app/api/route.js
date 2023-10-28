eb018724593ee46feb51616ba40d7a18eb5a3f0e
Lucia Wright <lucia.wright@student.manchester.ac.uk>
2023-10-28 16:31:39 +0100

    Heatmap Projection

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -7,3 +7,20 @@ export async function GET() {
     return res;
     
   }
+
+// The mapping between latitude, longitude and pixels is defined by the web
+// mercator projection.
+function project(lat,long) {
+    let siny = Math.sin((lat * Math.PI) / 180);
+  
+    // Truncating to 0.9999 effectively limits latitude to 89.189. This is
+    // about a third of a tile past the edge of the world tile.
+    siny = Math.min(Math.max(siny, -0.9999), 0.9999);
+  
+    return new google.maps.Point(
+      TILE_SIZE * (0.5 + long / 360),
+      TILE_SIZE * (0.5 - Math.log((1 + siny) / (1 - siny)) / (4 * Math.PI))
+    );
+  }
+
+  
