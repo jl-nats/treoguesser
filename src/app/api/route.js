c3001e5a144850b9d1f629c02f2909abbe363879
Lucia Wright <lucia.wright@student.manchester.ac.uk>
2023-10-28 16:03:22 +0100

    Getting heatmap responses

--- src/app/api/route.js
+++ src/app/api/route.js
@@ -1,8 +1,9 @@
-
-import characters from '@/data/characters.json'
 import { NextResponse } from 'next/server'
 
 
 export async function GET() {
-    return NextResponse.json({ characters: characters.data })
+    const res = await fetch(`https://airquality.googleapis.com/v1/mapTypes/UAQI_RED_GREEN/heatmapTiles/3/2/2?key=AIzaSyC5J5dQoUahFsI-C0SVOu0OlFLsrESGvpk`);
+    
+    return res;
+    
   }
