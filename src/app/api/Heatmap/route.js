e3f25cf6934b02edeeeee1f70998032f210ba3b5
Lucia Wright <lucia.wright@student.manchester.ac.uk>
2023-10-28 16:47:04 +0100

    Heatmap Translation

--- src/app/api/Heatmap/route.js
+++ src/app/api/Heatmap/route.js
@@ -2,6 +2,8 @@ import { NextResponse } from 'next/server'
 
 
 export async function GET() {
+    let point = {lat:53.4808, lng:2.2426};
+    let coord = project(point, 8);
     const res = await fetch(`https://airquality.googleapis.com/v1/mapTypes/UAQI_RED_GREEN/heatmapTiles/3/2/2?key=AIzaSyC5J5dQoUahFsI-C0SVOu0OlFLsrESGvpk`);
     
     return res;
@@ -10,17 +12,15 @@ export async function GET() {
 
 // The mapping between latitude, longitude and pixels is defined by the web
 // mercator projection.
-function project(lat,long) {
-    let siny = Math.sin((lat * Math.PI) / 180);
+function project(point,scale) {
+    
+    let siny = Math.sin((point.lat * Math.PI) / 180);
   
     // Truncating to 0.9999 effectively limits latitude to 89.189. This is
     // about a third of a tile past the edge of the world tile.
     siny = Math.min(Math.max(siny, -0.9999), 0.9999);
-  
-    return new google.maps.Point(
-      TILE_SIZE * (0.5 + long / 360),
-      TILE_SIZE * (0.5 - Math.log((1 + siny) / (1 - siny)) / (4 * Math.PI))
-    );
+    let coord = {lat:(scale * (0.5 + point.lng / 360)), lng: (scale * (0.5 - Math.log((1 + siny) / (1 - siny)) / (4 * Math.PI)))};
+    return coord;
   }
 
   
