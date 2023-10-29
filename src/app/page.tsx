cd725bcb8833c6fe416e9c5f2bd2a650b6582943
s38590mh <muhammad.hussain-14@student.manchester.ac.uk>
2023-10-29 10:01:05 +0000

    Add next button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,19 +12,33 @@ export default function Home() {
     Dispatch<SetStateAction<[string, number][]>>
   ] = useState([["NULL", 0]]);
 
+  const [pinOn, setPinOn] = useState(false);
+  const [loadMap, setLoadMap] = useState(true);
+
   return (
     <div className="relative">
       <section className="relative h-screen">
         <header className="flex relative text-8xl text-white drop-shadow-xl font-extrabold justify-center mt-10">
           Treoguessr
         </header>
-        <MapComponent setGameResult={setGameResult} />
+        <MapComponent
+          setGameResult={setGameResult}
+          pinOn={pinOn}
+          setPinOn={setPinOn}
+          loadMap={loadMap}
+        />
         <div
           className={`flex justify-center ${
-            gameResult.length > 1 ? " result-slide " : "opacity-0"
+            gameResult.length > 1 ? " result-slide " : " opacity-0 "
           }`}
         >
-          <ResultsComponent data={gameResult} />
+          <ResultsComponent
+            data={gameResult}
+            setData={setGameResult}
+            setPinOn={setPinOn}
+            setLoadMap={setLoadMap}
+            loadMap={loadMap}
+          />
         </div>
       </section>
       <section className="h-screen flex flex-wrap inset-y-10 bottom-0 bottom-100 bg-green-800 text-2xl text-white justify-center">
