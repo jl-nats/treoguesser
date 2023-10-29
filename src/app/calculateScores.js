2a9860acbde21867e0b432e23831b04dd03c2e34
Na Wang <na.wang-8@student.manchester.ac.uk>
2023-10-29 04:44:25 +0000

    Function converting user click location to longitude/latitude points

--- src/app/calculateScores.js
+++ src/app/calculateScores.js
@@ -1,8 +1,13 @@
 import {meanAQI} from "api/AirQualityData/route.js";
 import {getAQI} from "api/AirQualityData/route.js";
 
-function userEntrytoCoords(x,y){
-    
+function userEntrytoCoords(x,y,width,height){
+    let longitude = (x/(width/360))-100;
+    let mercN = ((height/2)-y)*((2*Math.PI)/width);
+    let lateralRadius = (mercN-Math.log(tan(Math.PI/4)))*2;
+    let latitude = lateralRadius*(100/Math.PI);
+
+    return longitude,latitude;
 }
 
 function calculateScores(longitude, latitude){
